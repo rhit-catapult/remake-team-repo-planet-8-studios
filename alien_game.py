c0a48d3af390901ad34338aae9c9c0d4fda37387
rhit-drummk <drummk@rose-hulman.edu>
2025-06-23 10:56:13 -0500

    Made working platforms; immediately shuts down

--- alien_game.py
+++ alien_game.py
@@ -1,5 +1,4 @@
-from Alien import Alien
-from Platform import Platform
+from alien import Alien
 import pygame
 import sys
 import alien
@@ -15,59 +14,71 @@ def main():
     # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((720, 560))
     # creates a Character from the alien.py file
-    character = alien.Character(screen, 100, 100)
+    alien = Alien(screen, 20, 400, "standing_sprite.png", "left_running.png", "left_lunging.png", "right_running.png", "right_lunging.png")
 
     # let's set the framerate
     clock = pygame.time.Clock()
 
-    alien = Alien(screen, 20, 400, "standing_sprite.png", "left_running.png", "left_lunging.png", "right_running.png", "right_lunging.png")
     IMAGE_HEIGHT = 80
     IMAGE_WIDTH = 56
 
     alien.stand = pygame.transform.scale(alien.stand, (IMAGE_WIDTH, IMAGE_HEIGHT))
 
-    gravity = 0.3
+    gravity = 0.8
     jump_speed = -14
     velocity_y = 0
     is_jumping = False
     ground_y = 400
 
+    obstacle1 = pygame.Rect(600, 400, 50, 50)  # x, y, width, height
+    obstacle2 = pygame.Rect(100, 400, 50, 50)
+    obstacles = [obstacle1, obstacle2]
+
     while True:
-        clock.tick(60)  # this sets the framerate of your game
+        clock.tick(60)  # set framerate
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            # TODO: Add you events code
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_LEFT]:
-            alien.x = alien.x-2
+            alien.x -= 2
         if pressed_keys[pygame.K_RIGHT]:
-            alien.x = alien.x+2
+            alien.x += 2
         if pressed_keys[pygame.K_UP]:
             if not is_jumping:
                 velocity_y = jump_speed
                 is_jumping = True
 
-            # apply gravity
+        # apply gravity
         velocity_y += gravity
         alien.y += velocity_y
 
-        # check if on ground
-        if alien.y >= ground_y:
+        # Alien rectangle for collision detection
+        on_obstacle = False
+        for obstacle in obstacles:
+            alien_rect = pygame.Rect(alien.x, alien.y, IMAGE_WIDTH, IMAGE_HEIGHT)
+            if alien_rect.colliderect(obstacle) and velocity_y >= 0:
+                # check if landing from above
+                if alien_rect.bottom >= obstacle.top and alien_rect.bottom - velocity_y < obstacle.top + 5:
+                    alien.y = obstacle.top - IMAGE_HEIGHT
+                    velocity_y = 0
+                    is_jumping = False
+                    on_obstacle = True
+                    break
+
+        # check if on the ground
+        if alien.y >= ground_y and not on_obstacle:
             alien.y = ground_y
-            is_jumping = False
             velocity_y = 0
+            is_jumping = False
 
-        # TODO: Fill the screen with whatever background color you like!
+        # draw everything
         screen.fill((255, 255, 255))
-
-        # draws the character every frame
+        pygame.draw.rect(screen, "green", obstacle1)
+        pygame.draw.rect(screen, "green", obstacle2)
         alien.draw()
-
-        # TODO: Add your project code
-
-        # don't forget the update, otherwise nothing will show up!
         pygame.display.update()
 
 
